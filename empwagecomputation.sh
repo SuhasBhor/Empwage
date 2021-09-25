52460b888d4cabe57049e7521137a1978d50b6af
SuhasBhor <suhasbhor5601@gmail.com>
2021-09-25 18:46:07 +0530

    [SuhasBhor] added:Empwagecal

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -4,11 +4,15 @@ echo "Welcome to EmpWage"
 empCheck=$((RANDOM%2))
 #constant variable
 IS_PRESENT=1
+emp_rate=20
+empHrs=8
 
 #selection
 if [ $empCheck -eq $IS_PRESENT ]
 then
+	salary=$(( empHrs * emp_rate ))
 	echo "emp is present"
 else
+	salary=0
 	echo "emp is absent"
 fi
