bf5eec795c58aba499632ef999c207588bd9248d
SuhasBhor <suhasbhor5601@gmail.com>
2021-09-25 18:27:11 +0530

    [SuhasBhor] added:EmpAttendance Feature

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,3 +1,14 @@
 #! /bin/bash -x
 
 echo "Welcome to EmpWage"
+empCheck=$((RANDOM%2))
+#constant variable
+IS_PRESENT=1
+
+#selection
+if [ $empCheck -eq $IS_PRESENT ]
+then
+	echo "emp is present"
+else
+	echo "emp is absent"
+fi
