14735d26436a4872cf10365832a8f38eea4c4b1b
SuhasBhor <suhasbhor5601@gmail.com>
2021-09-26 11:13:44 +0530

    [SuhasBhor] added:Empwagerefactorwithfunction

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -x
 
 echo "Welcome to EmpWage"
-empcheck=$(( RANDOM%3 ))
+
 is_present=1
 is_present_parttime=2
 emp_rate=20
@@ -10,18 +10,25 @@ total_work_hr=0
 max_working_hr=80
 day=1
 
-while(($day<=$total_working_day && $total_work_hr < $max_working_hr))
-do
-	empcheck=$((RANDOM%3))
-
+function getworkhr() {
+	local empcheck=$1
+	local emphrs=0
 	case $empcheck in
 	$is_present)
 	emphrs=8 ;;
 	$is_present_parttime)
 	emphrs=4 ;;
 	*)
-	emphrs=0 ;;
+	emphrs=0
 	esac
+	echo $emphrs
+}
+
+while(($day<=$total_working_day && $total_work_hr < $max_working_hr))
+do
+	empcheck=$((RANDOM%3))
+	#function call
+	emphrs=$( getworkhr $empcheck )
 	((day++))
 	total_work_hr=$(( total_work_hr + emphrs ))
 	salary=$(( emphrs * emp_rate ))
