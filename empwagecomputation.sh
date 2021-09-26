ffe6544aeec715f745554e6c252ce45cc8aa24cb
SuhasBhor <suhasbhor5601@gmail.com>
2021-09-26 10:48:31 +0530

    [SuhasBhor] added:Empwageformonth

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -5,13 +5,22 @@ empcheck=$(( RANDOM%3 ))
 is_present=1
 is_present_parttime=2
 emp_rate=20
+total_working_day=20
+total_work_hr=0
 
-case $empcheck in
-$is_present)
+for((day=1;day<=$total_working_day;day++))
+do
+	empcheck=$((RANDOM%3))
+
+	case $empcheck in
+	$is_present)
 	emphrs=8 ;;
-$is_present_parttime)
+	$is_present_parttime)
 	emphrs=4 ;;
-*)
+	*)
 	emphrs=0 ;;
-esac
-salary=$(( emphrs * emp_rate ))
+	esac
+	total_work_hr=$(( total_work_hr + emphrs ))
+	salary=$(( emphrs * emp_rate ))
+done
+totalsalary=$(( total_work_hr * emp_rate ))
