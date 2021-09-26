05e5e68f482b386b08c006d452f001a067121be0
SuhasBhor <suhasbhor5601@gmail.com>
2021-09-26 11:22:24 +0530

    [SuhasBhor] added:EmpstoredailywageArray

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -9,28 +9,22 @@ total_working_day=20
 total_work_hr=0
 max_working_hr=80
 day=1
+declare -a dailywage
 
-function getworkhr() {
-	local empcheck=$1
-	local emphrs=0
+while(($day<=$total_working_day && $total_work_hr < $max_working_hr))
+do
+	empcheck=$((RANDOM%3))
 	case $empcheck in
 	$is_present)
 	emphrs=8 ;;
 	$is_present_parttime)
 	emphrs=4 ;;
 	*)
-	emphrs=0
+	emphrs=0 ;;
 	esac
-	echo $emphrs
-}
-
-while(($day<=$total_working_day && $total_work_hr < $max_working_hr))
-do
-	empcheck=$((RANDOM%3))
-	#function call
-	emphrs=$( getworkhr $empcheck )
-	((day++))
 	total_work_hr=$(( total_work_hr + emphrs ))
-	salary=$(( emphrs * emp_rate ))
+	dailywage[$day]=$(( emphrs * emp_rate ))
+	((day++))
 done
 totalsalary=$(( total_work_hr * emp_rate ))
+echo wages:${dailywage[*]}
